390657dcc77898274bc3aea5a2459a68f7f5070b
PalkiaDude <teammegamanz@gmail.com>
2015-09-30 21:00:55 +0800

    Update Main.php

--- src/PalkiaDude/DeadEnd/Main.php
+++ src/PalkiaDude/DeadEnd/Main.php
@@ -16,7 +16,36 @@ class Main extends PluginBase implements Listener{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info(TextFormat::GREEN . "Dead-End Minigame by PalkiaDude!");
      }
-     public function onPlayerMove(PlayerMoveEvent $event){
+    public function onCommand(CommandSender $sender, Command $cmd, $label,array $args){
+        if(strtolower($cmd->getName()) === "footblock"){
+            if($sender instanceof Player){
+                if($this->isPlayer($sender)){
+                    $this->removePlayer($sender);
+                    $sender->sendMessage(TextFormat::GOLD . "You have left DeadEnd.");
+                    return true;
+                }
+                else{
+                    $this->addPlayer($sender);
+                    $sender->sendMessage(TextFormat::GREEN . "You have joined DeadEnd.");
+                    return true;
+                }
+            }
+            else{
+                $sender->sendMessage(TextFormat::RED . "Please use this command in-game.");
+                return true;
+            }
+        }
+    }
+    public function addPlayer(Player $player){
+        $this->players[$player->getName()] = $player->getName();
+    }
+    public function isPlayer(Player $player){
+        return in_array($player->getName(), $this->players);
+    }
+    public function removePlayer(Player $player){
+        unset($this->players[$player->getName()]);
+    }
+}     public function onPlayerMove(PlayerMoveEvent $event){
         $player = $event->getPlayer();
         $level = $player->getLevel();
         $pos = new Vector3($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ());
