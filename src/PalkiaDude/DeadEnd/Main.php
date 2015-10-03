5e28eb26a1a884bd1f304c04af9458a65c162674
PalkiaDude <teammegamanz@gmail.com>
2015-10-03 09:47:49 +0800

    Update Main.php

--- src/PalkiaDude/DeadEnd/Main.php
+++ src/PalkiaDude/DeadEnd/Main.php
@@ -46,12 +46,12 @@ public function onLobbyJoin(PlayerJoinEvent $event){
             if($sender instanceof Player){
                 if($this->isPlayer($sender)){
                     $this->removePlayer($sender);
-                    $sender->sendMessage(TextFormat::GOLD . "You have left DeadEnd.");
+                    $sender->sendMessage(TextFormat::GOLD . "You have left DeadEnd. Do /hub to go to hub.");
                     return true;
                 }
                 else{
                     $this->addPlayer($sender);
-                    $sender->sendMessage(TextFormat::GREEN . "You have joined DeadEnd.");
+                    $sender->sendMessage(TextFormat::GREEN . "You have joined DeadEnd. Only use this when in Arena.");
                     return true;
                 }
             }
@@ -74,73 +74,24 @@ public function onLobbyJoin(PlayerJoinEvent $event){
         $player = $event->getPlayer();
         $level = $player->getLevel();
         $pos = new Vector3($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ());
-        if($this->isPlayer($player)){
-            $level->setBlock($pos, 0);
+        $block = $ev->getPlayer()->getLevel()->getBlock($ev->getPlayer()->floor()->subtract(0, 1));
+  if($block->getId() === Block::GOLD_BLOCK){
+ $player->$pos->setBlock$block = $ev->getPlayer()->getLevel()->getBlock($ev->getPlayer()->floor()->subtract(0, 1));
+  if($block->getId() === Block::GOLD_BLOCK){
+  $event->$player->$pos->setBlock(new Vector3($x, $y, $z), Block::get(0))  }
+};
+  }
 }
-}
-     public function setGame(PlayerInteractEvent $event){
-$player=$event->getPlayer();
-$username=$player->getName();
-$block=$event->getBlock();
-$levelname=$player->getLevel()->getFolderName();
-if(isset($this->SetStatus[$username]))
-{
-switch ($this->SetStatus[$username])
-{
-case 0:
-if($event->getBlock()->getID() != 63 && $event->getBlock()->getID() != 68) 
-{
-$player->sendMessage(TextFormat::GREEN."please choose a sign to click on");
-return; 
-}
-$this->sign=array(
-"x" =>$block->getX(),
-"y" =>$block->getY(),
-"z" =>$block->getZ(),
-"level" =>$levelname);
-$this->config->set("sign",$this->sign);
-$this->config->save();
-$this->SetStatus[$username]++;
-$player->sendMessage(TextFormat::GREEN."sign has been created!");
-$player->sendMessage(TextFormat::GREEN."please click on the 1st spawnpoint");
-$this->signlevel=$this->getServer()->getLevelByName($this->config->get("sign")["level"]);
-$this->sign=new Vector3($this->sign["x"],$this->sign["y"],$this->sign["z"]);
-$this->changeStatusSign();
-break;
-case 1:
-$this->pos1=array(
-"x" =>$block->x,
-"y" =>$block->y,
-"z" =>$block->z,
-"level" =>$levelname);
-$this->config->set("pos1",$this->pos1);
-$this->config->save();
-$this->SetStatus[$username]++;
-$player->sendMessage(TextFormat::GREEN."Spawnpoint blue created");
-$player->sendMessage(TextFormat::GREEN."Please click on the red spawnpoint");
-$this->pos1=new Vector3($this->pos1["x"]+0.5,$this->pos1["y"],$this->pos1["z"]+0.5);
-break;
-case 2:
- $this->pos2=array(
-"x" =>$block->x,
-"y" =>$block->y,
-"z" =>$block->z,
-"level" =>$levelname);
-$this->config->set("pos2",$this->pos2);
-$this->config->save();
-$this->pos2=new Vector3($this->pos2["x"]+0.5,$this->pos2["y"],$this->pos2["z"]+0.5);
-$player->sendMessage(TextFormat::GREEN."Spawnpoint red created");
-$player->sendMessage(TextFormat::GREEN."All settings completed, you can start a game now.");
- $player->sendMessage(TextFormat::RED."REMINDER: MAKE SURE YOU DO /DE SETROOM AND TAP A REDSTONE BLOCK FOR WAITROOM!");
-  $player->sendMessage(TextFormat::RED."REMINDER: ALSO IT IS RECOMMENED THAT YOU RESTART THE SERVER");
-  unset($this->SetStatus[$username]);
-$this->level=$this->getServer()->getLevelByName($this->config->get("pos1")["level"]);
-break;
-}
-}
-}
+
+   public function onArenaJoin($event PlayerJoinEvent){
+    $arena = this->getConfig()->get("DeadEndArena");
+    $players = count($this->getServer()->getLevelByName($lobby)->getPlayers());    
+    
+
+    
    public function onDeath(PlayerDeathEvent $event){
-   $event->getPlayer()->removePlayer
+    $player = $event->getPlayer();   
+    $event->$player->removePlayer();
    $player->sendMessage(TextFormat::RED"You are out of the game!")
                                    
            }
