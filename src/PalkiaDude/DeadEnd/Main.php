68c29757900951ad9e6c65e3345fe9c53378d3fd
PalkiaDude <teammegamanz@gmail.com>
2015-10-04 00:35:43 +0800

    Update Main.php

--- src/PalkiaDude/DeadEnd/Main.php
+++ src/PalkiaDude/DeadEnd/Main.php
@@ -92,7 +92,7 @@ $timer = new Timer($this);
  $player->$pos->setBlock = $ev->getPlayer()->getLevel()->getBlock($ev->getPlayer()->floor()->subtract(0, 1));
 }
   if($block->getId() === Block::GOLD_BLOCK){
-  $event->$player->$pos->setBlock(new Vector3($x, $y, $z), Block::get(0))  
+  $event->$player->$pos->setBlock(new Vector3($x, $y, $z), Block::get(0)) }
 }
 
    public function onArenaJoin($event PlayerJoinEvent){
