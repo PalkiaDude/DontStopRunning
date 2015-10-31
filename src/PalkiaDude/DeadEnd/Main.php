e8fffdcdbb48ffa1cd9e69d770b87fa5dc972b61
PalkiaDude <teammegamanz@gmail.com>
2015-10-31 08:23:40 +0800

    HAH IM STILL ALIVE

--- src/PalkiaDude/DeadEnd/Main.php
+++ src/PalkiaDude/DeadEnd/Main.php
@@ -45,6 +45,17 @@ public function onLobbyJoin(PlayerJoinEvent $event){
    $this->getServer()->broadcastMessage("Too many players in lobby");
   }
 }
+public function FullLobby(PlayerJoinEvent $event){
+    $lobby = $this->getWorlds()->get("DeadEnd-lobby");
+  $targetlevel = $event->getTarget();
+  $players = count($this->getServer()->getLevelByName($lobby)->getPlayers());
+   if($targetlevel->$players > 10){
+      $sign = if($event->getBlock()->getId() == 63 or $event->getBlock()->getId() == 68) {
+            if($sign instanceof Sign) {
+                $signtext = $sign->getText();
+       if(TextFormat::clean($signtext[0]) === "[DeadEnd]") {  
+     $event->setLine(1,"GAME IS FULL!")
+      
 public function LobbyTimer(PlayerJoinEvent $event){
 $lobby = $this->getWorlds()->get("DeadEnd-lobby");
   $arena = $this->getWorlds()->get("DeadEndArena");
