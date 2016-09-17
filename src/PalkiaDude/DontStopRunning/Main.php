affc51e802b1eaa4603e4c3a5bb2a4d4337a499e
PalkiaDude <teammegamanz@gmail.com>
2016-09-18 00:39:00 +0800

    Added lives, the main concept of the game (partially) and the command (partially)
    Tomorrow I plan to finish up the main concept of the game and maybe the command.

--- src/PalkiaDude/DontStopRunning/Main.php
+++ src/PalkiaDude/DontStopRunning/Main.php
@@ -9,7 +9,52 @@ use pocketmine\block\Block;
 use pocketmine\Player;
 class Main extends PluginBase{
      
-     public function PluginEnable(){
+     public function onEnable(){
           $this->getLogger()->info("DontStopRunning v1 by DazAdam");
+   
      }
+     
+     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    if(strtolower($command->getName()) === "DSR"){
+         $sender->teleport(//to set spawn coordinates of lobby);
+        return true;
+    }
+
+    return false;
+}
+    public function PlayerMove($event PlayerMoveEvent){
+         $player = $event->getPlayer();
+         if($player->getLevel()->getName() === $this->getServer()->getLevelbyName("DSRArena")){
+            \//to be done
+         }
+    }
+    public function DeathSystem($event PlayerDeathEvent){
+         $lives = 5;
+         $player = $event->getPlayer();
+         if($player->getLevel()->getName() === $this->getServer()->getLevelbyName("DSRArena")){
+          if ($lives = 5){
+              $event->$lives--;
+              $player->sendMessage("You have 4 lives left!");
+          }
+          if ($lives = 4){
+              $event->$lives--;
+              $player->sendMessage("You have 3 lives left!");
+          }
+          if ($lives = 3){
+              $event->$lives--;
+              $player->sendMessage("You have 2 lives left!");
+              
+          }
+          if ($lives = 2){
+              $event->$lives--;
+              $player->sendMessage("This is your final life. What are last your words?");
+          }
+          if($lives = 1){
+              $event->$lives--;
+              $player->teleport(//spawn but I'm too lazy right now);
+              $player->sendMessage("You have lost the game. Better luck next time.");
+          }
+         }
+    }
+     
 }
