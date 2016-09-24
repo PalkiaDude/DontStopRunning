8a8c8cbb1027eb946bb0b9f4f291eb48c7018983
PalkiaDude <teammegamanz@gmail.com>
2016-09-24 10:33:34 +0800

    2/3 is done! Time for 3/3: Bug Fixing

--- src/PalkiaDude/DontStopRunning/Main.php
+++ src/PalkiaDude/DontStopRunning/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-// YOU NEED TO HAVE A WORLD NAMED DSRWORLD AND DSRLOBBY.
+// YOU NEED TO HAVE A WORLD NAMED DSRARENA AND DSRLOBBY.
 
 namespace PalkiaDude\DontStopRunning;
 use pocketmine\Server;
@@ -13,6 +13,7 @@ use pocketmine\Player;
 use pocketmine\level\Level;
 use pocketmine\command\Command;
 class Main extends PluginBase{
+     public $seconds;
      
      public function onEnable(){
           $this->getLogger()->info("DontStopRunning v2 by DazAdam");
@@ -32,6 +33,7 @@ class Main extends PluginBase{
     public function PlayerCount(PlayerJoinEvent $event){
          $level = getServer()->getLevelByName("DSRLobby");
          $playercount = $event->$level->getOnlinePlayers();
+         $player = $event->getPlayer();
          $arenaspawn = getServer()->getLevelByName("DSRArena")->getSpawnLocation();
          if($playercount === 1){
               $playercount->sendMessage("Countdown to game starts when 3 more players have joined.");
@@ -44,25 +46,60 @@ class Main extends PluginBase{
          }
          if($playercount === 4){
               $playercount->sendMessage("50 seconds to game!");
-              
-         }
-         if($playercount === 5){
-              
-         }
-         if($playercount === 6){
               $playercount->teleport($arenaspawn);
+               $task = new Task($this);
+               $this->$seconds = 0;
+               $h = $this->getServer()->getScheduler()->scheduleRepeatingTask($task, 1000);
+               $task->setHandler($h);
+               
          }
     }
-    public function onCompletion(Server $server){
-         
+    public function onRun($tick){
+         $this->$seconds++;
+         $level = getServer()->getLevelByName("DSRArena");
+         $playercount = $level->getOnlinePlayers();
+         if($seconds === 10){
+            $playercount->sendMessage("40 seconds left to game start");
+         }
+         if($seconds === 20){
+              $playercount->sendMessage("30 seconds left to game start");
+         }
+         if($seconds === 30){
+              $playercount->sendMessage("20 seconds left to game start");
+         }
+         if($seconds === 40){
+              $playercount->sendMessage("10 seconds left to game start");
+         }
+         if($seconds === 45){
+              $playercount->sendMessage("5 seconds left to game start");
+         }
+         if($seconds === 46){
+              $playercount->sendMessage("4 seconds left to game start");
+         }
+         if($seconds === 47){
+              $playercount->sendMessage("3 seconds left to game start");
+         }
+         if($seconds === 48){
+              $playercount->sendMessage("2 seconds left to game start");
+         }
+         if($seconds === 49){
+              $playercount->sendMessage("1 second left to game start");
+         }
+         if($seconds === 50){
+              $playercount->sendMessage("GAME HAS STARTED! RUUUUUNNNN"!);
+         }
     }
+    
     public function PlayerMove(PlayerMoveEvent $event)){
          $player = $event->getPlayer();
+         $x = $player->getFloorX();
+         $y = $player->getFloorY() -1;
+         $z = $player->getFloorZ();
          if($player->getLevel()->getName() === $this->getServer()->getLevelbyName("DSRArena")){
-            $block = getId(0);  
-        $pos = new Vector3($player->getFloorX(), $player->getFloorY() - 1, $player->getFloorZ());
-        if($this->isPlayer($player)){
-            $event->setBlock($pos, $block);
+            $block = getId(0);
+            $goldblock = getId(41);
+            if($y->getblockId($goldblock)){
+          $event->setblock($x, $y, $z, $block);
          }
     }
     }
